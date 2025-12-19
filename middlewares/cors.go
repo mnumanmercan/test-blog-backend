8c7d9cc53a7a71a38e73b1b8213cdfaad6d83938
Numan <numan.mercan24@gmail.com>
2025-12-20 00:22:29 +0300

    cors file env edit

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -1,11 +1,14 @@
 package middlewares
 
-import "net/http"
+import (
+	"net/http"
+	"os"
+)
 
 func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		w.Header().Set("Access-Control-Allow-Origin", os.Getenv("FRONTEND_URL"))
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
